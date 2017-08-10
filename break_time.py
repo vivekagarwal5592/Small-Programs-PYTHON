6245c7299c55a790f22c209d2bb6972f5a6b3b77
vivekagarwal5592 <vivekagarwal5592@gmail.com>
2017-08-10 15:36:08 -0700

    first commit. small python programs

---
+++ break_time.py
@@ -0,0 +1,9 @@
+import webbrowser
+import time
+
+i =0;
+while(i<2): 
+    time.sleep(10)
+    webbrowser.open("http://youtube.com")
+
+
---
+++ rename_files.py
@@ -0,0 +1,17 @@
+import os
+def rename_file():
+    file_list =os.listdir(r"C:\Users\user\Desktop\prank")
+    print(file_list)
+
+    for file_name in file_list
+       new_file_name = os.rename
+    
+        file_name=    file_name.translate(NONE,"1234567890")
+
+
+    print(file_list)
+
+
+
+
+rename_file()
